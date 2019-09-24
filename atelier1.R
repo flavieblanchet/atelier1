7e3ed7ebeb6b74e95003d5c212b80d5a8fc3da87
flavieblanchet <55747687+flavieblanchet@users.noreply.github.com>
2019-09-24 15:54:53 +0200

    Add files via upload

---
+++ atelier1.R
@@ -0,0 +1,27 @@
+2 + 3
+8 - 12
+14 * 25
+-3/10
+2.35/12.2
+1-sqrt(2)
+sort(c(2,5,1,3))
+2+3
+x <- 2
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles) 
+mean(tailles) #moyenne
+var(tailles) #variance
+sd(tailles) #écard-type
+summary(tailles) #résumé
+tailles.m <- tailles/100
+tailles.m
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+summary(poids)
+imc <- poids/tailles.m^2
+imc
+donnees <-data.frame(tailles,poids,imc)
